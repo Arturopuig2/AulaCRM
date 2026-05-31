90eb3b15abadf9f3c1fac886e91ab405140b77fe
Arturopuig2 <arturopuig2@gmail.com>
2026-05-31 15:49:02 +0200

    clean: remove unused source, build logs, and unreferenced resources

--- AulaCRM/test_pdf.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-import CoreData
-import UniformTypeIdentifiers
-
-// Fake Factura
-let ctx = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-
-// Compile test_pdf.swift with the rest of the project? No, it's easier to just build a mini project.
